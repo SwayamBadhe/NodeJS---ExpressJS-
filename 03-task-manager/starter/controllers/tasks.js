fd9440c75c391c3d06b2bda70a6d92ce3416ae19
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-09 20:46:16 +0530

    added controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 }
 
 const createTasks = (req, res) => {
-    res.send('Create task');
+    res.json(req.body);
 }
 
 const getTask = (req, res) => {
-    res.send('Single task');
+    res.json({id: req.params.id});
 }
 
 const updateTask = (req, res) => {
