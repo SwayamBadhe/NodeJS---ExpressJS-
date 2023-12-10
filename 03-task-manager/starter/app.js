13c12b3a08ca4019e5ccd94599196e888ded1a6e
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-10 17:28:58 +0530

    Add middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // middleware
 // pasrses incoming requests with JSON payloads
@@ -11,6 +12,8 @@ app.use(express.static('./public'))
 
 // routes
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const port = 3000;
 
